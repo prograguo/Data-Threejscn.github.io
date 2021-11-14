c0ecc3f83553e116cb55a951b472509d848480fb
prograguo <53160799+prograguo@users.noreply.github.com>
2021-11-14 21:26:20 +0800

    Update final-data-3js.js

--- js/final-data-3js.js
+++ js/final-data-3js.js
@@ -1,152 +1,131 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<title>three.js webgl - OBJLoader + MTLLoader</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
-		<link type="text/css" rel="stylesheet" href="main.css">
-	</head>
+import * as THREE from 'three.js-master/examples/build/three.module.js';
 
-	<body>
-		<div id="info">
-		<a href="https://threejs.org" target="_blank" rel="noopener">three.js</a> - OBJLoader + MTLLoader
-		</div>
+import { DDSLoader } from 'three.js-master/examples/jsm/loaders/DDSLoader.js';
+import { MTLLoader } from 'three.js-master/examples/jsm/loaders/MTLLoader.js';
+import { OBJLoader } from 'three.js-master/examples/jsm/loaders/OBJLoader.js';
 
-		<script type="module">
+var container;
 
-			import * as THREE from '../build/three.module.js';
+var camera, scene, renderer;
 
-			import { DDSLoader } from './jsm/loaders/DDSLoader.js';
-			import { MTLLoader } from './jsm/loaders/MTLLoader.js';
-			import { OBJLoader } from './jsm/loaders/OBJLoader.js';
+var mouseX = 0, mouseY = 0;
 
-			var container;
-
-			var camera, scene, renderer;
-
-			var mouseX = 0, mouseY = 0;
-
-			var windowHalfX = window.innerWidth / 2;
-			var windowHalfY = window.innerHeight / 2;
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
 
 
-			init();
-			animate();
+init();
+animate();
 
 
-			function init() {
+function init() {
 
-				container = document.createElement( 'div' );
-				document.body.appendChild( container );
+	container = document.createElement( 'div' );
+	document.body.appendChild( container );
 
-				camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-				camera.position.z = 250;
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.z = 250;
 
-				// scene
+	// scene
 
-				scene = new THREE.Scene();
+	scene = new THREE.Scene();
 
-				var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
-				scene.add( ambientLight );
+	var ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
+	scene.add( ambientLight );
 
-				var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
-				camera.add( pointLight );
-				scene.add( camera );
+	var pointLight = new THREE.PointLight( 0xffffff, 0.8 );
+	camera.add( pointLight );
+	scene.add( camera );
 
-				// model
+	// model
 
-				var onProgress = function ( xhr ) {
+	var onProgress = function ( xhr ) {
 
-					if ( xhr.lengthComputable ) {
+		if ( xhr.lengthComputable ) {
 
-						var percentComplete = xhr.loaded / xhr.total * 100;
-						console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
+			var percentComplete = xhr.loaded / xhr.total * 100;
+			console.log( Math.round( percentComplete, 2 ) + '% downloaded' );
 
-					}
+		}
 
-				};
+	};
 
-				var onError = function () { };
+	var onError = function () { };
 
-				var manager = new THREE.LoadingManager();
-				manager.addHandler( /\.dds$/i, new DDSLoader() );
+	var manager = new THREE.LoadingManager();
+	manager.addHandler( /\.dds$/i, new DDSLoader() );
 
-				// comment in the following line and import TGALoader if your asset uses TGA textures
-				// manager.addHandler( /\.tga$/i, new TGALoader() );
+	// comment in the following line and import TGALoader if your asset uses TGA textures
+	// manager.addHandler( /\.tga$/i, new TGALoader() );
 
-				new MTLLoader( manager )
-					.setPath( 'models/obj/male02/' )
-					.load( 'male02_dds.mtl', function ( materials ) {
+	new MTLLoader( manager )
+		.setPath( 'three.js-master/examples/models/obj/male02/' )
+		.load( 'male02_dds.mtl', function ( materials ) {
 
-						materials.preload();
+			materials.preload();
 
-						new OBJLoader( manager )
-							.setMaterials( materials )
-							.setPath( 'models/obj/male02/' )
-							.load( 'male02.obj', function ( object ) {
+			new OBJLoader( manager )
+				.setMaterials( materials )
+				.setPath( 'three.js-master/examples/models/obj/male02/' )
+				.load( 'male02.obj', function ( object ) {
 
-								object.position.y = - 95;
-								scene.add( object );
+					object.position.y = - 95;
+					scene.add( object );
 
-							}, onProgress, onError );
+				}, onProgress, onError );
 
-					} );
+		} );
 
-				//
+	//
 
-				renderer = new THREE.WebGLRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				container.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer();
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
 
-				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 
-				//
+	//
 
-				window.addEventListener( 'resize', onWindowResize, false );
+	window.addEventListener( 'resize', onWindowResize, false );
 
-			}
+}
 
-			function onWindowResize() {
+function onWindowResize() {
 
-				windowHalfX = window.innerWidth / 2;
-				windowHalfY = window.innerHeight / 2;
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
 
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
 
-				renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setSize( window.innerWidth, window.innerHeight );
 
-			}
+}
 
-			function onDocumentMouseMove( event ) {
+function onDocumentMouseMove( event ) {
 
-				mouseX = ( event.clientX - windowHalfX ) / 2;
-				mouseY = ( event.clientY - windowHalfY ) / 2;
+	mouseX = ( event.clientX - windowHalfX ) / 2;
+	mouseY = ( event.clientY - windowHalfY ) / 2;
 
-			}
+}
 
-			//
+//
 
-			function animate() {
+function animate() {
 
-				requestAnimationFrame( animate );
-				render();
+	requestAnimationFrame( animate );
+	render();
 
-			}
+}
 
-			function render() {
+function render() {
 
-				camera.position.x += ( mouseX - camera.position.x ) * .05;
-				camera.position.y += ( - mouseY - camera.position.y ) * .05;
+	camera.position.x += ( mouseX - camera.position.x ) * .05;
+	camera.position.y += ( - mouseY - camera.position.y ) * .05;
 
-				camera.lookAt( scene.position );
+	camera.lookAt( scene.position );
 
-				renderer.render( scene, camera );
+	renderer.render( scene, camera );
 
-			}
-
-		</script>
-
-	</body>
-</html>
+}
